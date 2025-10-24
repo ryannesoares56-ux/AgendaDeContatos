20cd0fc48b5525cf429dc6724ef653307f9439ad
ryannesoares56-ux <ryannesoares56@gmail.com>
2025-10-24 08:46:34 -0400

    Tela para adicionar contatos

---
+++ src/main/java/com/mycompany/agendadecontatos/AgendaDeContatos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.agendadecontatos;
+
+/**
+ *
+ * @author Aluno
+ */
+public class AgendaDeContatos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
