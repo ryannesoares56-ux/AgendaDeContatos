d275562fc8274eb32ecdc96ca638cc43c3afef58
ryannesoares56-ux <ryannesoares56@gmail.com>
2025-11-20 17:36:40 -0300

    Esse commit faz parte do "Criando classe Contato"
    Esqueci de dar Ctrl + S no NetBeans, e não salvou aqui. Aí não consegui reverter o commit anterior, dar erro.

--- src/main/java/com/mycompany/agendadecontatos/Contato.java
+++ src/main/java/com/mycompany/agendadecontatos/Contato.java
@@ -9,5 +9,51 @@ package com.mycompany.agendadecontatos;
  * @author ryann
  */
 public class Contato {
+    private int id;
+    private String nome;
+    private String telefone;
+    private String email;
+    
+    public Contato() {}
+
+    public Contato(int id, String nome, String telefone, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
     
 }
